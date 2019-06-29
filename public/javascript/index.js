7588cd5ba2edceed224abe6f0bddb1e0e57c4f35
Skufler <matviei.skufin@gmail.com>
2019-06-30 00:43:28 +0300

    add recommendation

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,7 @@ const serverIp = '192.168.43.119:8000';
 const myId = 1;
 
 $(document).on('input', '.slider', function () {
-   $(this).siblings('p').html($(this).val());
+   $(this).siblings('.value').html($(this).val());
 });
 let check = true;
 $(document).on('click','.user_evaluated',(e) => {
@@ -11,60 +11,3 @@ $(document).on('click','.user_evaluated',(e) => {
     check = !check;
     console.log(target);
 });
-
-fetch(`http://${serverIp}/eval/get/${myId}`, {mode: 'cors'})
-    .then((response) => response.json())
-    .then((response) => {
-        console.log(response.message);
-        const data = response.message.contacts;
-        data.map((user) => {
-            const {name, surname, uid} = user;
-            console.log(name, surname, uid);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-fetch(`http://${serverIp}/recommendation/new/${myId}`, {mode: 'cors'})
-    .then((response) => response.json())
-    .then((response) => {
-        console.log(response.message.recommendations);
-        const data = response.message.recommendations;
-        data.map((recommendation) => {
-            const {text} = recommendation;
-            console.log(text);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-fetch(`http://${serverIp}/eval/send/`, {
-    mode: 'cors',
-    method: 'post',
-    data: {
-        "uid": 2,
-        "evaluator": myId,
-        "date": "2016-07-25",
-        "partnership": 3,
-        "joint": 3,
-        "responsibility": 3,
-        "kindness": 3,
-        "trust": 3,
-        "anger": 3,
-        "irritability": 3,
-        "compliance": 3,
-        "sociopathy": 3,
-        "isolation": 3
-    },
-})
-    .then((response) => response.json())
-    .then((response) => {
-        console.log(response.status);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-return
--- server.js
+++ server.js
@@ -3,13 +3,62 @@ const app = express();
 const port = 3000;
 const serverIp = '192.168.43.119:8000';
 const myId = 1;
+const fetch = require('node-fetch');
+
+let dataEvaluatedUsers;
+let dataNewRecommendations;
 
 app.use('/public', express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>
-    res.sendfile('index.html')
-);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+fetch(`http://${serverIp}/eval/get/${myId}`, {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response.message);
+        dataEvaluatedUsers = response.message.contacts;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
+fetch(`http://${serverIp}/recommendation/new/${myId}`, {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response.message.recommendations);
+        dataNewRecommendations = response.message.recommendations;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+fetch(`http://${serverIp}/eval/send/`, {
+    mode: 'cors',
+    method: 'post',
+    data: {
+        "uid": 2,
+        "evaluator": myId,
+        "date": "2016-07-25",
+        "partnership": 3,
+        "joint": 3,
+        "responsibility": 3,
+        "kindness": 3,
+        "trust": 3,
+        "anger": 3,
+        "irritability": 3,
+        "compliance": 3,
+        "sociopathy": 3,
+        "isolation": 3
+    },
+})
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response.status);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.get('/', (req, res) => res.render('index', {dataEvaluatedUsers: dataEvaluatedUsers, dataNewRecommendations: dataNewRecommendations})
+);
