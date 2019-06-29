7a749151b674a0f1beb2371ffd12ac5e46cb040f
Skufler <matviei.skufin@gmail.com>
2019-06-29 21:18:24 +0300

    add fetch

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const serverIp = '192.168.43.119:8000';
+const myId = 1;
 
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>
     res.sendfile('index.html')
-)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
