f3960031f06e6d90140001601ddeafedaa7649e0
Tima Konev <1910tim@mail.ru>
2019-06-30 05:06:48 +0300

    merge 2 projects

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,10 +4,28 @@ const myId = 1;
 $(document).on('input', '.slider', function () {
    $(this).siblings('.value').html($(this).val());
 });
-let check = true;
-$(document).on('click','.user_evaluated',(e) => {
-    const target = e.currentTarget;
-    check ? $('.sidebar').css('width', '40%') : $('.sidebar').css('width', '0');
-    check = !check;
-    console.log(target);
+let check = false;
+let uid;
+let uidToServer = document.getElementsByClassName('uidToServer');
+$(document).on('click','.user_evaluated',function (e) {
+
+;    if(check===false){
+       uid = $(e.currentTarget).attr('id');
+       $('.sidebar').css('width', '40%');
+        $('.slider').val('0');
+     }
+    if(uid !==$(e.currentTarget).attr('id')){
+        $('.value').html('0');
+        $('.sidebar').css('width', '40%');
+        $('.slider').val('0');
+        uid = $(e.currentTarget).attr('id');
+    }
+    check = true;
 });
+
+    $(document).on('click','.submitButton',function (e) {
+        e.preventDefault();
+        uidToServer = uid;         //uidToServer id кнопки
+        $(`#${uid}`).remove()
+        $('.sidebar').css('width', '0');
+    })
